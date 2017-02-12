fe9ea3a28c2805ccd7ef52c502ce96864dc636da
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-12 22:40:33 +0900

    add chat_group_id to message

--- db/migrate/20170212132907_create_messages.rb
+++ db/migrate/20170212132907_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text :text
       t.references :user, foreign_key: true
+      t.references :chat_group, foreign_key: true
 
       t.timestamps
     end
