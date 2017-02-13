b2b413251cf81346aa82ff8af6f1c9da7c4e047f
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-13 16:53:08 +0900

    change timestamps in ChatGroups_table_migration_file as null: false

--- db/migrate/20170213074814_create_chat_groups.rb
+++ db/migrate/20170213074814_create_chat_groups.rb
@@ -2,7 +2,7 @@ class CreateChatGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :chat_groups do |t|
       t.string :name
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
