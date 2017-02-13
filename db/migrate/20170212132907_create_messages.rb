de7bcaddd6cc3e14a9323a367ebff858ce98f2c4
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-13 17:08:03 +0900

    Revert "add user_id to message"
    This reverts commit 122d74778771518cf67b8fb2a317fd808d1f0625.

--- db/migrate/20170212132907_create_messages.rb
+++ db/migrate/20170212132907_create_messages.rb
@@ -2,7 +2,6 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :text
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
