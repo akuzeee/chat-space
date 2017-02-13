aa0abe5eaef3c297c93a2fb8e74ffa814c014a92
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-13 18:18:13 +0900

    migrate to change name in chat_groups table as null: false

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213084306) do
+ActiveRecord::Schema.define(version: 20170213090536) do
 
   create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
