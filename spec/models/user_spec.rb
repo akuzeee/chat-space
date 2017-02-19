a9d9ef66610eae92f907e36509a2317bcbd85ff4
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-19 23:49:55 +0900

    Fix user#create_spec from english to japanese

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "is valid with all elements" do
+    it 'is valid with all elements' do
       user = build(:user)
       expect(user).to be_valid
     end
-    it "is invalid without a name" do
-      user = build(:user, name: "")
+    it 'is invalid without a name' do
+      user = build(:user, name: '')
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include('を入力してください。')
     end
-    it "is invalid without an email" do
-      user = build(:user, email: "")
+    it 'is invalid without an email' do
+      user = build(:user, email: '')
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include('を入力してください。')
     end
-    it "is invalid without a password" do
-      user = build(:user, password: "")
+    it 'is invalid without a password' do
+      user = build(:user, password: '')
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include('を入力してください。')
     end
-    it "is invalid without a password_confirmation although whth a password" do
-      user = build(:user, password_confirmation: "")
+    it 'is invalid without a password_confirmation although whth a password' do
+      user = build(:user, password_confirmation: '')
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません。')
     end
-    it "is valid when password has 8 characters or more " do
-      user = build(:user, password: "hogehoge", password_confirmation: "hogehoge")
+    it 'is valid when password has 8 characters or more ' do
+      user = build(:user, password: 'hogehoge', password_confirmation: 'hogehoge')
       expect(user).to be_valid
     end
-    it "is invalid when password has less than 8 characters" do
-      user = build(:user, password: "hogehog", password_confirmation: "hogehog")
+    it 'is invalid when password has less than 8 characters' do
+      user = build(:user, password: 'hogehog', password_confirmation: 'hogehog')
       user.valid?
-      expect(user.errors[:password][0]).to include("is too short")
+      expect(user.errors[:password][0]).to include('は8文字以上で入力してください。')
     end
-    it "is invalid when email has already existed" do
+    it 'is invalid when email has already existed' do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include('has already been taken')
+      expect(another_user.errors[:email]).to include('はすでに存在します。')
     end
   end
 end
