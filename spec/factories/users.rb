dc298d8bd68e484c4c85ef8abd9cecf877d32856
阿久澤圭 <akusawakei@MacBook-Pro-4.local>
2017-02-19 22:43:22 +0900

    Create factory_girl of user

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "abe"
+    email                 "kkk@gmail.com"
+    password              "aaaaaaaa"
+    password_confirmation "aaaaaaaa"
+  end
+end
