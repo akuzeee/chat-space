dec0c9f6b2dba6a1103d21559f54825a8911cb4f
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-22 14:55:16 +0900

    Create chat_groups table

--- db/migrate/20170222055155_create_chat_groups.rb
+++ db/migrate/20170222055155_create_chat_groups.rb
@@ -1,8 +1,8 @@
 class CreateChatGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :chat_groups do |t|
-
-      t.timestamps
+      t.string     :name, null: false
+      t.timestamps        null: false
     end
   end
 end
