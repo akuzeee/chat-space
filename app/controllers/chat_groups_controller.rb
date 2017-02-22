03850dcf2b84ecb8d683b3e033de3332526be545
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-23 01:06:22 +0900

    Define set_chat_group

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,4 +1,6 @@
 class ChatGroupsController < ApplicationController
+  before_action :set_chat_group, only: [:edit, :update]
+
   def new
     @chat_group = ChatGroup.new
   end
@@ -8,26 +10,26 @@ class ChatGroupsController < ApplicationController
     if @chat_group.save
       redirect_to root_path, notice: '新規グループを作成しました'
     else
-      flash[:alert] = 'グループ名を入力してください'
-      render new_chat_group_path
+      render :new
     end
   end
 
   def edit
-    @chat_group = ChatGroup.find(params[:id])
   end
 
   def update
-    @chat_group = ChatGroup.find(params[:id])
     if @chat_group.update(chat_group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
-      flash[:alert] = 'グループ名を入力してください'
-      render 'edit' #なぜかprefixでやると missing template になる
+      render :edit
     end
   end
 
   private
+  def set_chat_group
+    @chat_group = ChatGroup.find(params[:id])
+  end
+
   def chat_group_params
     params.require(:chat_group).permit(:name)
   end
