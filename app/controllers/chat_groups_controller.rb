c33a5f86cf16806c411080d7d6af8a1e9402602a
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-22 15:58:09 +0900

    Add create chat_group process

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,7 +1,20 @@
 class ChatGroupsController < ApplicationController
   def new
+    @chat_group = ChatGroup.new
+  end
+
+  def create
+    ChatGroup.create(create_params)
+    redirect_to :root and return
   end
 
   def edit
+    #TODO: ちゃんと取得する
+    @chat_group = ChatGroup.find(1)
+  end
+
+  private
+  def create_params
+    params.require(:chat_group).permit(:name)
   end
 end
