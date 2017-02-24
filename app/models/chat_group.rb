a8ca5cb3b6b96e5099970528b5e7d40ce14b3d10
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-24 11:02:36 +0900

    Add associations between chat_group and user

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -1,3 +1,6 @@
 class ChatGroup < ApplicationRecord
   validates_presence_of :name
+  has_many :group_members
+  has_many :users, through: :group_members
+  accepts_nested_attributes_for :group_members
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
+  has_many :group_members
+  has_many :chat_groups, through: :group_members
 end
