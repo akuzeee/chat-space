be48c0b6109a7cd74902ccb00512053c86feab9f
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-25 15:32:45 +0900

    Rename redirect_method for nonmember

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,8 +1,8 @@
 class ChatGroupsController < ApplicationController
-  before_action :set_chat_group,  only: %i(show edit update)
-  before_action :set_chat_groups, only: %i(show index)
-  before_action :set_users,       only: %i(new edit create update)
-  before_action :redirect,        only: %i(show edit)
+  before_action :set_chat_group,   only: %i(show edit update)
+  before_action :set_chat_groups,  only: %i(show index)
+  before_action :set_users,        only: %i(new edit create update)
+  before_action :reject_nonmember, only: %i(show edit)
 
   def index
   end
@@ -52,7 +52,7 @@ class ChatGroupsController < ApplicationController
     params.require(:chat_group).permit(:name, user_ids: [])
   end
 
-  def redirect
+  def reject_nonmember
     redirect_to :root unless GroupMember.exists?(chat_group_id: params[:id], user_id: current_user.id)
   end
 end
