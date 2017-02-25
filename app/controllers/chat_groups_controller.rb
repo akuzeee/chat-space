858f841d17ba275bf5ef6a29d622654b840c1c84
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-25 12:13:50 +0900

    Update redirect after create and update

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -17,7 +17,7 @@ class ChatGroupsController < ApplicationController
   def create
     @chat_group = ChatGroup.new(chat_group_params)
     if @chat_group.save
-      redirect_to root_path, notice: '新規グループを作成しました'
+      redirect_to chat_group_path(@chat_group), notice: '新規グループを作成しました'
     else
       render :new
     end
@@ -28,7 +28,7 @@ class ChatGroupsController < ApplicationController
 
   def update
     if @chat_group.update(chat_group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to chat_group_path(@chat_group), notice: 'グループを更新しました'
     else
       render :edit
     end
