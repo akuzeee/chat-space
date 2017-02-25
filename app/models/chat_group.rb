c329f371a880bfe9e06ad88229070f2406c04eed
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-25 13:14:22 +0900

    Change error_message when group_member is blank

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -1,6 +1,6 @@
 class ChatGroup < ApplicationRecord
   validates_presence_of :name
-  validates_acceptance_of :user_ids
+  validates_acceptance_of :users, message: 'を選択してください'
   has_many :group_members
   has_many :users, through: :group_members
 end
