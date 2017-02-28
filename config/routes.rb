aa43f8be2a8fd9a87dc5e8903373c3d371382ec4
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-28 12:36:09 +0900

    Add routes related to message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root       'chat_groups#index'
-  resources  :chat_groups, except: :destroy
+  resources  :chat_groups, except: :destroy do
+    resources :messages, only: %i(index create)
+  end
 end
