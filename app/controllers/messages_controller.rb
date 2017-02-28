ed266f56a2065b7cab0127921d0a8d88bce409ba
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-28 17:31:04 +0900

    Add message posting actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,15 @@ class MessagesController < ApplicationController
   before_action :set_all_chat_groups,    only: :index
   before_action :set_current_chat_group, only: :index
   before_action :reject_nonmember,       only: :index
+  before_action :set_message,            only: :index
 
   def index
     render 'chat_groups/index'
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to chat_group_messages_path(params[:chat_group_id])
   end
 
   private
@@ -18,5 +24,13 @@ class MessagesController < ApplicationController
 
   def reject_nonmember
     redirect_to :root unless current_user.join_in?(@chat_group)
+  end
+
+  def set_message
+    @message = Message.new
+  end
+
+  def message_params
+    params.require(:message).permit(:text).merge(chat_group_id: params[:chat_group_id], user_id: current_user.id)
   end
 end
