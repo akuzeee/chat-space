68f8667481a141cf8efdf2c89b59c5e4eda7eed8
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-02-28 11:43:56 +0900

    Migrate to create messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223131450) do
+ActiveRecord::Schema.define(version: 20170227130926) do
 
   create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20170223131450) do
     t.datetime "updated_at",    null: false
     t.index ["chat_group_id"], name: "index_group_members_on_chat_group_id", using: :btree
     t.index ["user_id"], name: "index_group_members_on_user_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "text"
+    t.integer  "user_id",       null: false
+    t.integer  "chat_group_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["chat_group_id"], name: "index_messages_on_chat_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -48,4 +58,6 @@ ActiveRecord::Schema.define(version: 20170223131450) do
 
   add_foreign_key "group_members", "chat_groups"
   add_foreign_key "group_members", "users"
+  add_foreign_key "messages", "chat_groups"
+  add_foreign_key "messages", "users"
 end
