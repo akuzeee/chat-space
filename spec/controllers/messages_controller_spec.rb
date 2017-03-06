d1f26108acae82aa7f428f4445adf0ab5e1eaf15
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-06 19:13:41 +0900

    Update messages_controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,28 +2,54 @@ require 'rails_helper'
 
 describe MessagesController do
   describe 'GET #index' do
+    let(:user) { create(:user) }
+
     context 'with logged in user' do
+      before do
+        login_user
+        allow(controller).to receive(:current_user).and_return(user)
+      end
+
       context 'when current_user belongs to @chat_group' do
+        let(:chat_group) { create(:chat_group, user_ids: user.id) }
+        before do
+          get :index, chat_group_id: chat_group
+        end
+
         it "renders the :index template" do
+          expect(response).to render_template :index
         end
 
         it "assigns the requested contact to @chat_group" do
+          expect(assigns(:chat_group)).to eq chat_group
         end
 
         it "assigns the requested contact to @chat_groups" do
-        end
-
-        it "assigns the requested contact to @message" do
+          chat_groups = user.chat_groups.includes(:messages).all
+          expect(assigns(:chat_groups)).to eq chat_groups
         end
       end
 
       context 'when current_user does not belongs_to @chat_group' do
-        it 'redirects to chat_groups/:index' do
+        let(:chat_group) { create(:chat_group) }
+        before do
+          get :index, chat_group_id: chat_group
+        end
+
+        it 'redirects and havs 302 status' do
+          expect(response).to have_http_status(302)
         end
       end
     end
+
     context 'without logged in user' do
-      it "redirects to devise/sessions/:new " do
+      let(:chat_group) { create(:chat_group, user_ids: user.id) }
+      before do
+        get :index, chat_group_id: chat_group
+      end
+
+      it 'redirects and has 302 status' do
+        expect(response).to have_http_status(302)
       end
     end
   end
