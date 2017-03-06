e7af934900a73ee26674dc8f79ba9e5bdc70e091
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-06 22:31:34 +0900

    Add #create to messages_controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -30,7 +30,7 @@ describe MessagesController do
         end
       end
 
-      context 'when current_user does not belongs_to chat_group' do
+      context 'when current_user does not belong_to chat_group' do
         it 'redirects to root_path' do
           chat_group = create(:chat_group)
           get :index, params: { chat_group_id: chat_group }
@@ -50,26 +50,44 @@ describe MessagesController do
   describe 'POST #create' do
     let(:user) { create(:user) }
     let(:chat_group) { create(:chat_group, user_ids: user.id) }
-
     before do
       login_user
       allow(controller).to receive(:current_user).and_return(user)
     end
 
-    context 'when message.text exists' do
+    context 'with valid attributes' do
+      let(:message_attributes) { attributes_for(:message, chat_group_id: chat_group.id, user_id: user.id) }
+
       it 'redirects_to chat_groups_messages_path' do
-        post :create, params: { chat_group_id: chat_group, message: attributes_for(:message, chat_group_id: chat_group.id, user_id: user.id) }
+        post :create, params: { chat_group_id: chat_group, message: message_attributes }
         expect(response).to redirect_to chat_group_messages_path(chat_group)
       end
 
       it 'saves the new contact in the database' do
         expect{
-          post :create, params: { chat_group_id: chat_group, message: attributes_for(:message, chat_group_id: chat_group.id, user_id: user.id) }
+          post :create, params: { chat_group_id: chat_group, message: message_attributes }
         }.to change(Message, :count).by(1)
       end
     end
 
-    context 'when message.text empty' do
+    context 'with an empty text attribute' do
+      let(:message_attributes) { attributes_for(:message, text: '', chat_group_id: chat_group.id, user_id: user.id) }
+
+      it 're-renders the :index template' do
+        post :create, params: { chat_group_id: chat_group, message: message_attributes }
+        expect(response).to render_template :index
+      end
+
+      it 'does not save the new contact' do
+        expect{
+          post :create, params: { chat_group_id: chat_group, message: message_attributes }
+        }.to_not change(Message,:count)
+      end
+
+      it 'contains flash message' do
+        post :create, params: { chat_group_id: chat_group, message: message_attributes }
+        expect(flash.now[:alert]).not_to be_empty
+      end
     end
   end
 end
