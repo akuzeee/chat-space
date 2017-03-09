71ccb6507a09d22ae89af334e714f91465a7ff60
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-09 21:48:03 +0900

    Add 500ms time lag

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,6 +1,7 @@
 $(function() {
   var searchResult = $('#user-search-result');
   var preInput;
+  var preFunc;
 
   function appendSearchedList(user) {
     var item = $('<div class="chat-group-user searched-user clearfix">').append('<p class="chat-group-user__name">' + user.name);
@@ -17,29 +18,34 @@ $(function() {
     return reg;
   }
 
-  $('#user-search-field').on('keyup', function() {
+  ajaxPost = function(input) {
     var selectedUserName = $('.chat-group-user__name--selected').text().replace(/\r?\n/g,"");
     var selectedUserNames = splitWord(selectedUserName);
     var regSelectedUserNames = convertToReg(selectedUserNames);
+    $('.searched-user').remove();
+    $.ajax({
+    type: 'GET',
+    url: 'search',
+    data: ('q=' + input),
+    dataType: 'json'
+    })
+    .done(function(data) {
+      if (input.length !== 0) {
+        $.each(data.searched_users, function(i, user) {
+          if (!user.name.match(regSelectedUserNames)) {
+           appendSearchedList(user);
+         }
+        });
+      }
+    });
+  };
+
+  $('#user-search-field').on('keyup', function() {
     // 半角スペースを無視して検索
     var input = $('#user-search-field').val().replace(/ /g, '');
     if (input != preInput) {
-      $('.searched-user').remove();
-      $.ajax({
-      type: 'GET',
-      url: 'search',
-      data: ('q=' + input),
-      dataType: 'json'
-      })
-      .done(function(data) {
-        if (input.length !== 0) {
-          $.each(data.searched_users, function(i, user) {
-            if (!user.name.match(regSelectedUserNames)) {
-             appendSearchedList(user);
-           }
-          });
-        }
-      })
+      clearTimeout(preFunc);
+      preFunc = setTimeout(ajaxPost(input), 500);
     }
     preInput = input;
   });
