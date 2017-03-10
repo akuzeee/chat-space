e2c791f4fa58c6072a4f468327883a407cce4e16
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-10 22:48:58 +0900

    Clean descriptions

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,9 +1,8 @@
 $(function() {
   function buildHTML(message) {
+    var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>');
     if (message.image.url) {
-      var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>', '<img src=' + message.image.url + '>');
-    } else {
-      var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>');
+      html.append('<img src=' + message.image.url + '>');
     }
     return html;
   }
@@ -14,15 +13,14 @@ $(function() {
     var params = new FormData($('#new_message').get(0));
     var path = location.pathname;
     $.ajax({
-      type: 'POST',
-      url: path,
+      type:        'POST',
+      url:         path,
       contentType: false,
       processData: false,
-      data: params,
-      dataType: 'json'
+      data:        params,
+      dataType:    'json'
     })
     .done(function(data) {
-      console.log(data.image)
       var html = buildHTML(data);
       $('.chat-messages').append(html);
       textField.val('');
