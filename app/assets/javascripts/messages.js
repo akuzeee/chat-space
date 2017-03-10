1ad749755f5ae60945ad0e5979d27cda3bc81461
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-10 22:39:53 +0900

    Update image uploading function with ajax

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,25 +1,28 @@
 $(function() {
   function buildHTML(message) {
-    var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>');
+    if (message.image.url) {
+      var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>', '<img src=' + message.image.url + '>');
+    } else {
+      var html = $('<li class="chat-message">').append('<p class="chat-message__user-name">' + message.user.name + '</p>', '<span class="chat-message__date">' + message.created_at + '</span>', '<div class="chat-message__body">' + message.text + '</div>');
+    }
     return html;
   }
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var textField = $('#message_body');
-    var text = textField.val();
+    var params = new FormData($('#new_message').get(0));
     var path = location.pathname;
     $.ajax({
       type: 'POST',
       url: path,
-      data: {
-        message: {
-          text: text
-        }
-      },
+      contentType: false,
+      processData: false,
+      data: params,
       dataType: 'json'
     })
     .done(function(data) {
+      console.log(data.image)
       var html = buildHTML(data);
       $('.chat-messages').append(html);
       textField.val('');
