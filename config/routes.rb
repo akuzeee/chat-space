ad7ebbef162faca7d683311669a1d3702f9a164a
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-10 12:38:56 +0900

    Using resources and collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/search' => 'users#search', format: 'json'
+  resources :users, except: %i(index create new edit show update destroy) do
+    collection do
+      get :search, format: 'json'
+    end
+  end
   devise_for :users
   root       'chat_groups#index'
   resources  :chat_groups, except: %i(show destroy) do
