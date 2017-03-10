dac0a77474d39788838edb6a80827d700723e436
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-10 17:27:50 +0900

    Revert "Avoid bug caused by turbolinks in management.js"
    This reverts commit a75c742f5dc3a119c8c1624775b7e49ff1409b6e.

--- app/assets/javascripts/chat_group_members_management.js
+++ app/assets/javascripts/chat_group_members_management.js
@@ -1,21 +1,24 @@
-// 追加ボタンでsearchedUserをselectedUserに移動
-$(document).on('click', '.chat-group-user__btn--add', function() {
+$(function() {
   var selectedList = $('#chat-group-users')
   function appendSelectedList(user) {
     var item = $('<div class="chat-group-user clearfix" data-user-id=' + user.id + '>').append('<input name="chat_group[user_ids][]", type="hidden", value=' +user.id + ' >', '<p class="chat-group-user__name">' + user.name, '<a class="chat-group-user__btn chat-group-user__btn--remove">削除');
     selectedList.append(item);
   }
-  var userBlock = $(this).parent();
-  var selectedUser = {
-    name: userBlock.find('p').text(),
-    // searchedUserが動的な要素のためかdatasetにアクセスできないのでattrを使う
-    id: userBlock.attr('data-user-id')
-  };
-  userBlock.remove();
-  appendSelectedList(selectedUser);
-});
 
-// 削除ボタンでselectedUserを削除
-$(document).on('click', '.chat-group-user__btn--remove', function() {
-  $(this).parent().remove();
+  // 追加ボタンでsearchedUserをselectedUserに移動
+  $(document).on('click', '.chat-group-user__btn--add', function() {
+    var userBlock = $(this).parent();
+    var selectedUser = {
+      name: userBlock.find('p').text(),
+      // searchedUserが動的な要素のためかdatasetにアクセスできないのでattrを使う
+      id: userBlock.attr('data-user-id')
+    };
+    userBlock.remove();
+    appendSelectedList(selectedUser);
+  });
+
+  // 削除ボタンでselectedUserを削除
+  $(document).on('click', '.chat-group-user__btn--remove', function() {
+    $(this).parent().remove();
+  });
 });
