dcf3a6ec90b0ba3b67f43495ba307b3e74a8451b
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-10 18:35:41 +0900

    Clean descriptions

--- app/assets/javascripts/chat_group_members_management.js
+++ app/assets/javascripts/chat_group_members_management.js
@@ -1,5 +1,6 @@
-$(document).on('turbolinks:load',function() {
-  var selectedList = $('#chat-group-users')
+$(document).on('turbolinks:load', function() {
+  var selectedList = $('#chat-group-users');
+
   function appendSelectedList(user) {
     var item = $('<div class="chat-group-user chat-group-user--selected clearfix" data-user-id=' + user.id + '>').append('<input name="chat_group[user_ids][]", type="hidden", value=' +user.id + ' >', '<p class="chat-group-user__name">' + user.name, '<a class="chat-group-user__btn chat-group-user__btn--remove">削除');
     selectedList.append(item);
