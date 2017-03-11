def09f5dbeae669439ee714e74078511e75a5a4b
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-11 21:08:13 +0900

    Revert "Add rmagick gem to resize image"
    This reverts commit d1a39a8e6ecef40bef183c02304b3617efa48ee0.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  process resize_to_limit: [480, 360]
-
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
