2bab99356c1f92c07cf659f9da31e11487dfaecc
阿久澤圭 <akusawakei@MacBook-Pro-5.local>
2017-03-12 18:57:25 +0900

    Fix typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region: 'ap-northeaset-1'
+    region: 'ap-northeast-1'
   }
 
   case Rails.env
